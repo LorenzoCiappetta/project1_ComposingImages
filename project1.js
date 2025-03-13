25e19c4806debaf3aaa6c9ec2d2be09a7f0a9c53
Lorenzo Ciappetta <ciappetta.2011296@studenti.uniroma1.it>
2025-03-13 18:10:16 +0100

    started project

--- project1.js
+++ project1.js
@@ -4,4 +4,18 @@
 // fgPos is the position of the foreground image in pixels. It can be negative and (0,0) means the top-left pixels of the foreground and background are aligned.
 function composite( bgImg, fgImg, fgOpac, fgPos )
 {
+    //alert(fgPos.x);
+    var w = bgImg.width;
+    var h = bgImg.height;
+    
+    var data = bgImg.data;
+    
+    for(var i = 0; i < w; i++){
+        for(var j = 0; j < h; j++){
+            for(var c = 0; c < 3; c++){
+                //data[i * j * c] = 0 //fgImg.data[i, j, c];
+            }
+        }
+    }
+    
 }
